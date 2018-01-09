713fb87e0e846ac93577464dc37b2446744b5f15
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-09 21:29:03 +0530

    vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.>
+#include<conio.h>
+void main()
+{
+int c,lowercase,uppercase;
+printf("\n enter the vowel")
+scanf("%d",&c)
+lowercase(c='a'||c='e'||c='i'||c='o'||c='u')
+uppercase(C='A'||C='E'||C='I'||C='O'||C='U')
+if(lowercase||uppercase)
+printf("\n it is vowels");
+else
+printf("\n consonant");
+return 0;
+}
