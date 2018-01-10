1b23cac2db8223eb5fb946894084836c878ab38f
rameshd1998 <30828554+rameshd1998@users.noreply.github.com>
2018-01-10 20:38:59 +0530

    alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("\n enter the variable")
+scanf("%c",&ch)
+if(ch>=='a'&&ch<=='z')||(ch>=='A'&&ch<=='Z')
+printf("enter the character %cis an alphabet",ch);
+else
+printf("enter the character t%c is not alphabet",ch);
+return 0;
+}
